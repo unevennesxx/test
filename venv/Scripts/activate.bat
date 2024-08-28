8cc54431ce1282ead835fd5656ec08260c49f98f
UnevennesX <appreciaty@gmail.com>
2024-08-28 17:14:40 -0500

    update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\UnevennesS\Desktop\TEST\venv
+set VIRTUAL_ENV=C:\Users\UnevennesS\Documents\test\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
